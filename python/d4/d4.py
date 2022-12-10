65dcb8b5c01b3aa3501ff60cc45df7acf32ba8ee
minifixio <emile@le-gallic.com>
2022-12-10 19:13:54 +0100

    python - day4 (p1)

---
+++ python/d4/d4.py
@@ -0,0 +1,13 @@
+f = open("./data/d4/input.txt")
+l = [ x.split(',') for x in ''.join(f.readlines()).strip().split('\n') ]
+
+def p1():
+    s=0
+    for e in l:
+        a=list(map(int,e[0].split('-')))
+        b=list(map(int,e[1].split('-')))
+        if (a[0]<=b[0] and b[1]<=a[1]) or (b[0]<=a[0] and a[1]<=b[1]):
+            s+=1
+    return s
+
+p1()
