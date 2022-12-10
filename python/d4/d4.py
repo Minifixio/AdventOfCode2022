4318d24925d93ab4fd7b223b920474fd8eb8a10d
minifixio <emile@le-gallic.com>
2022-12-10 19:21:09 +0100

    python - day 4 (p2)

--- python/d4/d4.py
+++ python/d4/d4.py
@@ -10,4 +10,14 @@ def p1():
             s+=1
     return s
 
+def p2():
+    s=0
+    for e in l:
+        a=list(map(int,e[0].split('-')))
+        b=list(map(int,e[1].split('-')))
+        if (a[0]<=b[0] and b[0]<=a[1]) or (b[0]<=a[0] and a[0]<=b[1]):
+            s+=1
+    return s
+
 p1()
+p2()
